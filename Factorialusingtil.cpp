22ad16ccea7034999df0a63610d2c43d43670648
darpanhh <162995199+darpanhh@users.noreply.github.com>
2025-06-16 22:39:49 +0545

    Create Factorialusingtil.cpp

---
+++ Factorialusingtil.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// Tail recursive function
+int factorialTail(int n, int result = 1) {
+    if (n == 0 || n == 1)
+        return result;
+    return factorialTail(n - 1, n * result);
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number < 0)
+        cout << "Factorial is not defined for negative numbers." << endl;
+    else
+        cout << "Factorial of " << number << " is " << factorialTail(number) << endl;
+
+    return 0;
+}
